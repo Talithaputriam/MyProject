0f12dcbaa5fd97f4771ae350ccb341401fc77055
Talitha Putri Ameilia <96828290+Talithaputriam@users.noreply.github.com>
2021-12-31 15:13:35 +0700

    Add files via upload

---
+++ soal 1.py
@@ -0,0 +1,6 @@
+def Money_Convertion(Gold):
+    konversi = Gold*930000
+    print("Gold price per gram to Rupiah:",konversi,"Rupiah")
+# SID = 1201204454
+Gold = float(input("input the nominal (gr): "))
+Money_Convertion(Gold)
